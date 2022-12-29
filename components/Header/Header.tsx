bc9801622e71f2544435ae88d605ef63d25beb0d
henrique <henrique.almeida231@hotmail.com>
2022-12-29 10:44:23 -0300

    Rotas do menu criada

--- components/Header/Header.tsx
+++ components/Header/Header.tsx
@@ -1,6 +1,11 @@
+import Link from 'next/link'
+import { useRouter } from 'next/router';
 import styles from './Header.module.scss'
 
 export default function Header() {
+  const router = useRouter();
+  const rota = router.asPath
+  console.log(rota)
   return (
     <>
     <header className={styles.header}>
@@ -8,10 +13,18 @@ export default function Header() {
         <nav className={styles.navsdektop}>
             <div className={styles.line}></div>
             <ul>
-                <li><span className={styles.number}>00</span> <span className={styles.name}>HOME</span></li>
-                <li><span className={styles.number}>01</span> <span className={styles.name}>DESTINATION</span></li>
-                <li><span className={styles.number}>02</span> <span className={styles.name}>CREW</span></li>
-                <li><span className={styles.number}>03</span> <span className={styles.name}>TECHNOLOGY</span></li>
+                <Link href='/home' className={styles.link} style={rota == '/home' ? {borderBottom: '1px solid white'} : {}}>
+                  <li><span className={styles.number}>00</span> <span className={styles.name}>HOME</span></li>
+                </Link>
+                <Link href='/destination' className={styles.link} style={rota == '/destination' ? {borderBottom: '1px solid white'} : {}}>
+                  <li><span className={styles.number}>01</span> <span className={styles.name}>DESTINATION</span></li>
+                </Link>
+                <Link href='/crew' className={styles.link} style={rota == '/crew' ? {borderBottom: '1px solid white'} : {}}>
+                  <li><span className={styles.number}>02</span> <span className={styles.name}>CREW</span></li>
+                </Link>
+                <Link href='/technology' className={styles.link} style={rota == '/technology' ? {borderBottom: '1px solid white'} : {}}>
+                  <li><span className={styles.number}>03</span> <span className={styles.name}>TECHNOLOGY</span></li>
+                </Link>
             </ul>
         </nav>
     </header>

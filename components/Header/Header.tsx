a43c288394652c7be15da937f32c960e232cc8ba
henrique <henrique.almeida231@hotmail.com>
2022-12-31 16:45:40 -0300

    menumobile

--- components/Header/Header.tsx
+++ components/Header/Header.tsx
@@ -1,10 +1,14 @@
 import Link from 'next/link'
 import { useRouter } from 'next/router';
+import { useRef, useState } from 'react';
 import styles from './Header.module.scss'
 
 export default function Header() {
   const router = useRouter();
   const rota = router.asPath
+
+  const [menuopen, setMenuopen] = useState(false);
+
   return (
     <>
     <header className={styles.header}>
@@ -22,6 +26,24 @@ export default function Header() {
                   <li><span className={styles.number}>02</span> <span className={styles.name}>CREW</span></li>
                 </Link>
                 <Link href='/technology' className={styles.link} style={rota == '/technology' ? {borderBottom: '1px solid white'} : {}}>
+                  <li><span className={styles.number}>03</span> <span className={styles.name}>TECHNOLOGY</span></li>
+                </Link>
+            </ul>
+        </nav>
+        <img src='menuhamburger.png' className={styles.mobileclick} onClick={()=>{setMenuopen(true)}}></img>
+        <nav className={menuopen == false ? styles.navsmobileclose : styles.navsmobileopen}>
+            <img src='closemenu.png' className={styles.menuclose} onClick={()=>{setMenuopen(false)}}></img>
+            <ul>
+                <Link href='/home' className={styles.link}>
+                  <li><span className={styles.number}>00</span> <span className={styles.name}>HOME</span></li>
+                </Link>
+                <Link href='/destination/moon' className={styles.link}>
+                  <li><span className={styles.number}>01</span> <span className={styles.name}>DESTINATION</span></li>
+                </Link>
+                <Link href='/crew' className={styles.link}>
+                  <li><span className={styles.number}>02</span> <span className={styles.name}>CREW</span></li>
+                </Link>
+                <Link href='/technology' className={styles.link}>
                   <li><span className={styles.number}>03</span> <span className={styles.name}>TECHNOLOGY</span></li>
                 </Link>
             </ul>
